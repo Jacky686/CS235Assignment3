9d08610444216b54d1ca118a1b413d34006ed75d
Jacky <jche686@aucklanduni.ac.nz>
2020-10-30 23:30:16 +1300

    Added more database testing

--- tests/integration/test_orm.py
+++ tests/integration/test_orm.py
@@ -33,6 +33,41 @@ def make_user():
     return user
 
 
+def insert_movie(empty_session, values=None):
+    new_title = "Ice Age"
+    new_release_year = 2002
+    new_description = "The ice age"
+    new_runtime_minutes = 120
+    new_external_rating = 8.5
+    new_rating_votes = 5000
+    new_revenue_in_millions = 100
+    new_metascore = 88
+
+    if values is not None:
+        new_title = values[0]
+        new_release_year = values[1]
+        new_description = values[2]
+        new_runtime_minutes = values[3]
+        new_external_rating = values[4]
+        new_rating_votes = values[5]
+        new_revenue_in_millions = values[6]
+        new_metascore = values[7]
+
+    empty_session.execute(
+        'INSERT INTO movies (title, release_year, description, runtime_minutes, external_rating, rating_votes, revenue_in_millions, metascore) VALUES (:title, :release_year, :description, :runtime_minutes, :external_rating, :rating_votes, :revenue_in_millions, :metascore)',
+        {'title': new_title, 'release_year': new_release_year, 'description': new_description,
+         'runtime_minutes': new_runtime_minutes, 'external_rating': new_external_rating,
+         'rating_votes': new_rating_votes, 'revenue_in_millions': new_revenue_in_millions, 'metascore': new_metascore})
+    row = empty_session.execute('SELECT id from movies where title = :title',
+                                {'title': new_title}).fetchone()
+    return row[0]
+
+
+def make_movie():
+    movie = Movie("Ice Age", 2002)
+    return movie
+
+
 def test_loading_of_users(empty_session):
     users = list()
     users.append(("andrew", "1234"))
@@ -55,3 +90,10 @@ def test_saving_of_users(empty_session):
     assert rows == [("andrew", "111")]
 
 
+def test_saving_of_movies(empty_session):
+    movie = make_movie()
+    empty_session.add(movie)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT title, release_year FROM movies'))
+    assert rows == [("Ice Age", 2002)]
